3f906c63bbb8ff1e082ef6108eb550473a90974e
SebaChuaqui <schuaquicano@gmail.com>
2022-05-27 17:37:38 -0300

    Estudio Herencia V

---
+++ figgeometricaes6/script.js
@@ -0,0 +1,18 @@
+class FiguraGeometrica {
+    constructor(lado) {
+        this.lado = lado;
+    }
+}
+
+class Poligono extends FiguraGeometrica {
+    constructor(lado) {
+        super(lado);
+    }
+    calcularArea(){
+        return this.lado * this.lado;
+    }        
+}
+
+var cuadrado = new Poligono(3);
+console.log(cuadrado);
+console.log(cuadrado.calcularArea());
